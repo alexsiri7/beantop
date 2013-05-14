92ab432678a538e4fdb98b5959cea115ddc4ff70
Alex <alexsiri7@gmail.com>
2013-05-14 17:35:15 +0100

    Initial refactor of BeanstalkdStats class

--- beantop/beanstalkdstats.py
+++ beantop/beanstalkdstats.py
@@ -6,29 +6,33 @@ class BeanstalkdStats:
     TUBE_STATS_FIELDS = ["current-jobs-delayed", "current-jobs-ready", "current-jobs-reserved", "current-waiting"]
     def __init__(self, b):
         self.b=b
-    def stats(self):
+    def _stats(self):
         return self.format(self.b.yaml_data_filtered("stats", self.GLOBAL_STATS_FIELDS))
         
     def _getTubes(self):
         return self.b.yaml_data("list-tubes")
         
-    def tubestats(self):
+    def _getFilteredTubes(self):
         tubes = self._getTubes()
-        t_data = ""
         ts = dict()
         for t in tubes:
             d = self.b.yaml_data_filtered("stats-tube "+t, self.TUBE_STATS_FIELDS)
             if (d["current-jobs-delayed"]+d["current-jobs-reserved"]+d["current-jobs-ready"]>0):
                 ts[t]=d
+        return ts
+        
+    def _tubestats(self):
+        ts = self._getFilteredTubes()
+        t_data = ""
         max_items = 5
-            
         rest = ts
         while len(rest)>0:
             ts = dict(rest.items()[:max_items])
             rest = dict(rest.items()[max_items:])
             t_data += self.renderRow(ts)
         return t_data
-    def renderRow(self,  ts):
+        
+    def _renderRow(self,  ts):
         header_width = 25
         field_width = 15
         t_data = "Tube".ljust(header_width)
@@ -46,7 +50,8 @@ class BeanstalkdStats:
       status = self.stats()
       tubestatus = self.tubestats()
       return [status, tubestatus]
-    def format(self,dic):
+      
+    def _format(self,dic):
       ret_data=""
       for k,v in dic.iteritems():
         ret_data+=k+": "+str(v)+"\n"
