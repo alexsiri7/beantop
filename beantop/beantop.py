6181ea270023dca50cfad8a5b04add3b94fbdf6b
Alex <alexsiri7@gmail.com>
2013-06-04 10:02:23 +0100

    Fixed problem with arguments parsing

--- beantop/beantop.py
+++ beantop/beantop.py
@@ -3,7 +3,7 @@ import sys
 
 def main():
     arguments = factory.create_arguments_parser()
-    host, port = arguments.process(sys.argv)
+    host, port = arguments.process(sys.argv[1:])
     beanstalkd,  console = factory.start_application(host, port)
     beanstalkd.connect()
     console.main_loop()
